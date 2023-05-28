19c00a9642bc4a6925c979c4cc58339fdc8cf5df
Konstantin Panyushkin <k.panyushkin@playneta.gg>
2023-05-29 00:46:28 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
 
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3000', 'https://shop-client-vercel.vercel.app/'],
+    origin: ['http://localhost:3001', 'https://shop-client-vercel.vercel.app/'],
   });
 
   const config = new DocumentBuilder()
